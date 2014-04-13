46f408a839a1aac3cb918bb1decd288675ae9076
Richo Healey <richo@psych0tik.net>
2014-04-13 08:36:44 -0700

    Update CLI to work with new API

--- example/cli.rs
+++ example/cli.rs
@@ -4,62 +4,42 @@ extern crate stripe;
 extern crate http;
 use stripe::connection::Connection;
 use http::method::Get;
-use http::headers::HeaderEnum;
 
 use std::os;
-use std::str;
-use std::io::println;
+
+fn usage() {
+    let args = os::args();
+    println!("Usage: {} <record type>", args[0]);
+    println!("  Record types:");
+    println!("  - customers");
+}
 
 fn main() {
     format!("{}", Get);
     let args = os::args();
     match args.len() {
         0 => unreachable!(),
-        1 => make_and_print_request(),
+        2 => fetch_and_print_records(args[1]),
         _ => {
-            println!("Usage: {} URL", args[0]);
+            usage();
             return;
         },
     };
 }
 
-fn make_and_print_request() {
-    // let url = authenticatedUrl(~"/v1/customers");
-    // let mut request: RequestWriter = RequestWriter::new(Get, url).unwrap();
-    // request.headers.insert(ExtensionHeader(~"Authorization", format!("Bearer {}", secretKey())));
-
+fn fetch_and_print_records(typ: ~str) {
     let secretKey: ~str = os::getenv("STRIPE_SECRET_KEY").expect("No STRIPE_SECRET_KEY set");
     let conn = Connection::new(secretKey);
-    let request = conn.customers();
+    let mut data;
 
-    println!("[33;1mRequest[0m");
-    println!("[33;1m=======[0m");
-    println!("");
-    println!("[1mURL:[0m {}", request.url.to_str());
-    println!("[1mRemote address:[0m {:?}", request.remote_addr);
-    println!("[1mMethod:[0m {}", request.method);
-    println!("[1mHeaders:[0m");
-    for header in request.headers.iter() {
-        println!(" - {}: {}", header.header_name(), header.header_value());
+    if typ == ~"customers" {
+        data = conn.customers()
+    } else {
+        usage();
+        return;
     }
 
-    println!("");
-    println!("[33;1mResponse[0m");
-    println!("[33;1m========[0m");
-    println!("");
-    let mut response = match request.read_response() {
-        Ok(response) => response,
-        Err(_request) => fail!("This example can progress no further with no response :-("),
-    };
-    println!("[1mStatus:[0m {}", response.status);
-    println!("[1mHeaders:[0m");
-    for header in response.headers.iter() {
-        println!(" - {}: {}", header.header_name(), header.header_value());
+    for item in data.iter() {
+        println!("{}", item);
     }
-    println!("[1mBody:[0m");
-    let body = match response.read_to_end() {
-        Ok(body) => body,
-        Err(err) => fail!("Reading response failed: {}", err),
-    };
-    println(str::from_utf8(body.as_slice()).expect("Uh oh, response wasn't UTF-8"));
 }
