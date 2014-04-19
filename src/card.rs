48aeedbb1ee8b42a3452d0b309568b212c9790c1
Richo Healey <richo@psych0tik.net>
2014-04-19 16:52:58 -0700

    Populate card struct

--- src/card.rs
+++ src/card.rs
@@ -7,4 +7,24 @@ pub struct CardList {
 }
 
 #[deriving(Decodable,Show)]
-pub struct Card;
+pub struct Card {
+    object: ~str,
+    id: ~str,
+    last4: ~str,
+    exp_month: uint,
+    exp_year: uint,
+    fingerprint: ~str,
+    customer: Option<~str>, // Customerlist, but I'm not sure how that works
+                            // with circular references
+    country: ~str,
+    name: Option<~str>,
+    address_line1: Option<~str>,
+    address_line2: Option<~str>,
+    address_city: Option<~str>,
+    address_state: Option<~str>,
+    address_zip: Option<~str>,
+    address_country: Option<~str>,
+    cvc_check: ~str,
+    address_line1_check: ~str,
+    address_zip_check: ~str
+}
